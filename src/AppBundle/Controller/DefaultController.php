a09e145db7dc66fa3b7e83f26abf2fafa3652c68
didimed <medhi971@hotmail.com>
2016-11-01 22:59:00 +0100

    Home templating

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,10 +12,39 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
-    {
+    {   
+        $articles = [
+            ['q_name' => 'test'],
+            ['q_name' => 'test2'],
+            ['q_name' => 'test3'],
+        ];
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'articles' => $articles,
         ]);
+    }
+
+    /**
+     * @Route("/", name="test")
+     */
+    public function testAction(Request $request)
+    {   
+       
+    }
+
+    /**
+     * @Route("/", name="test2")
+     */
+    public function test2Action(Request $request)
+    {   
+       
+    }
+
+    /**
+     * @Route("/", name="test3")
+     */
+    public function test3Action(Request $request)
+    {   
+       
     }
 }
